9ac50aafe6dc985cff7de2ede0bb000b26a24d04
adore <71892513@qq.com>
2017-06-11 22:22:17 +0800

    my third commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-//helloa
-app.listen(3000, function () {
-    console.log('3000!')
-});
+// const express = require('express');
+// const app = express();
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+// //helloa
+// app.listen(3000, function () {
+//     console.log('3000!')
+// });
+console.log('124');
